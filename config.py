8a7af47766d6221081dca5768590b16cf8c2be87
Root-X-Admin <ayushhande052@gmail.com>
2025-03-05 05:51:20 +0530

    sad

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+    SECRET_KEY = 'your_secret_key_here'
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Flask-Mail Configuration
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = 'ashande29@gmail.com'
+    MAIL_PASSWORD = 'yequetiqbwqyztyc'
+    # MAIL_DEFAULT_SENDER = 'ashande29@gmail.com'
+    MAIL_USE_SSL = True
