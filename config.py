c904b7ba33518d6116b5ae6069c0969ffeb601c7
Root-X-Admin <ayushhande052@gmail.com>
2025-06-15 15:03:25 +0530

    Refactor config.py to load environment variables for sensitive configurations

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
-import os
 import psycopg2
+import os
+from dotenv import load_dotenv
 
+load_dotenv()  # Make sure to load .env variables
 
 class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = ""
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Flask-Mail Configuration
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
-    MAIL_DEFAULT_SENDER = ''
-    MAIL_USE_SSL = False
-
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_TYPE = 'filesystem'
+
+    # Flask-Mail Configuration (Brevo)
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False') == 'True'
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
